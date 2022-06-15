5c74b46bcf174db4dab200550d5daac8e846c435
Gnol0909 <thanhlong31190910@gmail.com>
2022-06-15 16:23:49 +0700

    update#cre/editapi

--- src/context/Api.context.js
+++ src/context/Api.context.js
@@ -2,6 +2,40 @@ import axios from "axios";
 
 export default class API {
     // no params
+    async createProduct(name,price,description,slug,category,col,sty,detail,disc,disc_percent,thumb,clas,listIma ) {
+        let a = await axios.post(`http://localhost:4001/product/create`,{
+            product_name: name,
+            product_price: price,
+            description: description,
+            slug: slug,
+            categories: category,
+            color: col,
+            style: sty,
+            detail_info: detail,
+            discount: disc,
+            discount_percent: disc_percent,
+            thumbnail: thumb,
+            classify: clas,
+            listImage: listIma
+        })
+        return a;
+    }
+    async updateProduct(name,price,description,slug,category,col,sty,detail,disc,disc_percent,clas ) {
+        let a = await axios.post(`http://localhost:4001/product/create`,{
+            product_name: name,
+            product_price: price,
+            description: description,
+            slug: slug,
+            categories: category,
+            color: col,
+            style: sty,
+            detail_info: detail,
+            discount: disc,
+            discount_percent: disc_percent,
+            classify: clas
+        })
+        return a;
+    }
     async getTotalbyMonth(yea,mon) {
         let a = await axios.get(`http://localhost:4001/salesstatus/total/`+yea+`/`+mon) 
         return a.data;
